c39bf757bb1e130041a2ca433d5fbd2b33ab4e27
Thomaz de Souza Scopel <106708501+thomazscopel@users.noreply.github.com>
2025-06-30 14:37:03 +0000

    atualizando funcoes de adicionar e remover, e terminando a logica principal do projeto de tarefas

--- Projeto/Source/main.swift
+++ Projeto/Source/main.swift
@@ -15,50 +15,102 @@ func showMenu() {
     """, terminator: "")
 }
 
-func adicionarTarefa(lista: [String]) {
-    print("Digite sua nova tarefa: ")
-    guard let tarefa = readLine() else {
-        return
+func mostrarTarefas(lista: [String]) {
+    print("\n--- Suas Tarefas ---")
+    if !lista.isEmpty {
+        for (index, tarefa) in lista.enumerated() {
+            print("\(index). \(tarefa)")
+        }
+    } else {
+        print("Nenhuma tarefa na lista.")
     }
-    lista.append(tarefa)
+    print("--------------------")
 }
 
-func removerTarefa(lista: [String]) {
+
+func adicionarTarefa(lista: inout [String]) {
     
-    let indice: Int = -1
+    var adiciona = true
     
+    // Repete enquanto o usuario quiser adicionar para multiplas tarefas
     repeat {
-        // To Do: chamar funcao de mostrar tarefas aqui
-        print("Digite o número da tarefa que deseja remover: ", terminator: "")
-        guard let input = readLine() else {
+        mostrarTarefas(lista: lista)
+        print("Digite sua nova tarefa ou digite 'v' para voltar: ")
+        guard var linha = readLine() else {
+            print("Nenhuma entrada identificada! Retornando ao Menu.")
             return
         }
         
-        if let numIndice = Int(input) {
-            indice = numIndice
-            
-            // Se o indice esta dentro do intervalo de itens da lista
-            if indice > -1 && indice < lista.count {
-                lista.remove(at:indice)
-                print("Tarefa removida com sucesso!!")
-                return
-            }
-            
-            print("Opção Inválida! Deve estar no intervalo de itens!")
+        // Faz o tratamento da entrada
+        linha = linha.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if linha.lowercased() == "v" {
+            adiciona = false
+        } else if linha.isEmpty { // Verifica se eh uma entrada vazia
+            print("A tarefa não pode ser vazia! Por favor digite algo.")
         } else {
-            print("Entrada inválida! Por favor, digite um número.")
-            indice = -1
+            let tarefa = linha
+            lista.append(tarefa)
         }
-    } while indice < 0 && indice > lista.count
+        
+    } while adiciona
+}
+
+func removerTarefa(lista: inout [String]) {
+    
+    var remove = true
+    
+    // Repete enquanto o usuario quiser remover para multiplas tarefas
+    repeat {
+        if (lista.isEmpty) {
+            print("Nada para remover, lista vazia!!")
+            return
+        }
+        
+        mostrarTarefas(lista: lista) 
+        print("Digite o número da tarefa que deseja remover ( ou 'v' para voltar): ", terminator: "")
+        
+        guard var linha = readLine() else {
+            print("Nenhuma entrada identificada! Retornando ao Menu.")
+            return
+        }
+        
+        // Faz o tratamento da entrada
+        linha = linha.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if linha.lowercased() == "v" {
+            remove = false
+        } else if linha.isEmpty { // Verifica se eh entrada vazia
+            print("A entrada não pode ser vazia! Por favor digite algo.")
+        } else {
+            if let indiceTarefaRemovida = Int(linha) { // Verifica se a entrada foi um numero
+                // Verifica se o indice esta dentro do intervalo do array
+                if indiceTarefaRemovida >= 0 && indiceTarefaRemovida < lista.count {
+                    lista.remove(at: indiceTarefaRemovida)
+                } else {
+                    print("Indice inexistente! Digite no intervalo de tarefas!")
+                }
+            } else {
+                print("Entrada inválida! Por favor digite um número!")
+            }
+        }
+        
+    } while remove
     
 }
 
+// FIM DE FUNCOES
+
+// main()
+
 var opcao: Int = 0 
+var listaTarefas: [String] = []
 
 // Loop para verificar uma entrada válida
 repeat {
     showMenu()
     guard let input = readLine() else {
+        print("Nenhuma entrada identificada, encerrando programa!")
         exit(0) 
     }
 
@@ -68,16 +120,24 @@ repeat {
         
         if opcao < 1 || opcao > 5 {
             print("Opção Inválida! Deve inserir um número entre 1 e 5.")
+        } else {
+            switch opcao {
+                case 1: adicionarTarefa(lista: &listaTarefas)
+                case 2: removerTarefa(lista: &listaTarefas)
+                case 3: mostrarTarefas(lista: listaTarefas)
+                case 4: print("To do")
+                case 5:
+                    print("Encerrando programa...")
+                    exit(0)
+                default:
+                    print("Comportamento inesperado. Reiniciando Menu..")
+            }
         }
     } else {
         // Se a entrada não for um número, imprime erro e mantém 'opcao' inválida
         print("Entrada inválida! Por favor, digite um número.")
-        opcao = 0 
     }
+    
+    opcao = 0 // Atribui valor fora do intervalo para permanencia do loop
 
 } while opcao < 1 || opcao > 5 // O loop continua enquanto a opcao for inválida
-
-
-
-
-
